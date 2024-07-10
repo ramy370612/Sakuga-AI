424d77118e0dc8c971082c55eddb8a56263d8c75
k-arikawa0312 <s1F102402710@iniad.org>
2024-07-10 18:27:33 +0900

    add search window and link to each works

--- client/pages/index.page.tsx
+++ client/pages/index.page.tsx
@@ -1,24 +1,49 @@
-import { useRouter } from "next/router";
-import { useMemo } from "react";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
 
 const Home = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   // `/?search=hoge` -> `['hoge']` のように、searchパラメータを配列で取得
   // 検索するキーワードをsearchパラメータとして保持し、それをもとに検索を行う
   const searchParams = useMemo(() => {
-    const searchParam = router.query.search
-    return Array.isArray(searchParam) ? searchParam : searchParam !== undefined ? [searchParam] : []
-  }, [router.query.search])
+    const searchParam = router.query.search;
+    return Array.isArray(searchParam)
+      ? searchParam
+      : searchParam !== undefined
+        ? [searchParam]
+        : [];
+  }, [router.query.search]);
 
+  const novels = [
+    {
+      id: 'test',
+      title: 'test',
+      author: 'test',
+      imageUrl: 'https://st.cdjapan.co.jp/pictures/l/12/19/NEOBK-2110612.jpg',
+    },
+  ];
+  const urls = novels.map((novel) => `novel/${novel.id}`);
   return (
-    <div>
-      <h1>Sakuga AI</h1>
-      {searchParams.length > 0 && (
-        <p>Search: {searchParams.join(', ')}</p>
-      )}
-    </div>
+    <>
+      <div>
+        <h1>Sakuga AI</h1>
+        {searchParams.length > 0 && <p>Search: {searchParams.join(', ')}</p>}
+        <input type="search" />
+        <button>検索</button>
+      </div>
+      <label>ランキング</label>
+      <div>
+        <Link href={urls[0]}>走れメロス</Link>
+        <Link href={urls[0]}>羅生門</Link>
+      </div>
+      <div>
+        <Link href={urls[0]}>吾輩は猫である</Link>
+        <Link href={urls[0]}>山月記</Link>
+      </div>
+    </>
   );
-}
+};
 
 export default Home;
