d6bf6975779c99618a0e963dd811aeb28f957ef8
Sho0226 <s1F102201616@iniad.org>
2024-07-11 01:55:17 +0900

    add: ranking

---
+++ server/tests/api/novels/ranking.test.ts
@@ -0,0 +1,24 @@
+import aspida from '@aspida/axios';
+import api from 'api/$api';
+import assert from 'assert';
+import axios from 'axios';
+import { API_BASE_PATH, PORT } from 'service/envValues';
+import { expect, test } from 'vitest';
+import { GET } from '../utils';
+
+const baseURL = `http://127.0.0.1:${PORT}${API_BASE_PATH}`;
+
+const apiAxios = axios.create({ withCredentials: true });
+
+const apiClient = api(aspida(apiAxios, { baseURL }));
+
+test(GET(apiClient.novels.ranking), async () => {
+  const res = await apiClient.novels.ranking.$get({ query: { limit: 54 } });
+  assert(res !== null);
+  //書き出しが一致することを確認
+  expect(res[0].title).toEqual('こころ');
+
+  const nullRes = await apiClient.novels.ranking.$get({ query: { limit: 0 } });
+  //存在しないworkIdの場合、nullが返ることを確認
+  expect(nullRes).toBeNull();
+});
