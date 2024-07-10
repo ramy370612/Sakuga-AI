d5ec1e6c871c509dbd2ba7d178ecce3a88df3d72
Yoshiki <s1f102201469@iniad.org>
2024-07-10 14:19:21 +0900

    feat: add test for novels api

---
+++ server/tests/api/novels/text.test.ts
@@ -0,0 +1,29 @@
+import aspida from '@aspida/axios';
+import api from 'api/$api';
+import axios from 'axios';
+import { API_BASE_PATH, PORT } from 'service/envValues';
+import { expect, test } from 'vitest';
+import { GET } from '../utils';
+
+const baseURL = `http://127.0.0.1:${PORT}${API_BASE_PATH}`;
+
+const apiAxios = axios.create({ withCredentials: true });
+
+const apiClient = api(aspida(apiAxios, { baseURL }));
+
+test(GET(apiClient.novels.text), async () => {
+  const res = await apiClient.novels.text.$get({ query: { workId: 1567 } });
+  //書き出しが一致することを確認
+  expect(res).toContain('走れメロス');
+});
+
+test(GET(apiClient.novels.text), async () => {
+  const res = await apiClient.novels.text.$get({ query: { workId: 15681231 } });
+  //存在しないworkIdの場合、nullが返ることを確認
+  expect(res).toBeNull();
+});
+
+test(GET(apiClient.novels), async () => {
+  const res = await apiClient.novels.$get();
+  expect(res).toEqual('');
+});
