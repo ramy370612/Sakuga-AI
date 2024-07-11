5d0cb0b433b0c5029295188f8494ba833665cc80
Yoshiki <s1f102201469@iniad.org>
2024-07-11 19:38:11 +0900

    feat: Update novelUseCase to use novelQuery

--- server/domain/novel/useCase/novelUseCase.ts
+++ server/domain/novel/useCase/novelUseCase.ts
@@ -1,14 +1,14 @@
 import { load } from 'cheerio';
 import { decode } from 'iconv-lite';
 import { transaction } from 'service/prismaClient';
-import { getNovelUrlByWorkId } from '../repository/novelQuery';
+import { novelQuery } from '../repository/novelQuery';
 
 export const novelUseCase = {
   gettext: async (workId: number): Promise<string | null> =>
     transaction('RepeatableRead', async (tx) => {
-      const novelURL = await getNovelUrlByWorkId(tx, workId);
-      if (novelURL === null) return null;
-      const buffer = await fetch(novelURL).then((b) => b.arrayBuffer());
+      const novel = await novelQuery.getNovelByWorkId(tx, workId);
+      if (novel === null) return null;
+      const buffer = await fetch(novel.aozoraUrl).then((b) => b.arrayBuffer());
       const html = decode(Buffer.from(buffer), 'Shift_JIS');
       const $ = load(html);
 
