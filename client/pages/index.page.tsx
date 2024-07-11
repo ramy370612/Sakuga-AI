8e19600b40f26ea5d3008ec207499b8230eebac2
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-07-11 21:21:52 +0900

    add(client): fetch ranking from api (`/novels/ranking`)

--- client/pages/index.page.tsx
+++ client/pages/index.page.tsx
@@ -1,11 +1,18 @@
-import Link from 'next/link';
+import { useCatchApiErr } from 'hooks/useCatchApiErr';
 import { useRouter } from 'next/router';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { apiClient } from 'utils/apiClient';
 import styles from './index.module.css';
 
+type UnwrapPromise<T extends Promise<unknown>> = T extends Promise<infer U> ? U : never;
+
 const Home = () => {
-  const router = useRouter();
+  const [rankings, setRankings] = useState<
+    UnwrapPromise<ReturnType<typeof apiClient.novels.ranking.$get>>
+  >([]);
   const [searchInput, setSearchInput] = useState('');
+  const router = useRouter();
+  const catchApiErr = useCatchApiErr();
 
   // `/?search=hoge` -> `['hoge']` のように、searchパラメータを配列で取得
   // 検索するキーワードをsearchパラメータとして保持し、それをもとに検索を行う
@@ -18,71 +25,18 @@ const Home = () => {
         : [];
   }, [router.query.search]);
 
-  const novels = [
-    {
-      id: 'test1',
-      title: '走れメロス',
-      author: '太宰治',
-      imageUrl: 'https://placehold.jp/150x150.png',
-    },
-    {
-      id: 'test2',
-      title: '羅生門',
-      author: '芥川龍之介',
-      imageUrl: 'https://placehold.jp/150x150.png',
-    },
-    {
-      id: 'test3',
-      title: '吾輩は猫である',
-      author: '夏目漱石',
-      imageUrl: 'https://placehold.jp/150x150.png',
-    },
-    {
-      id: 'test4',
-      title: '吾輩は猫である',
-      author: '夏目漱石',
-      imageUrl: 'https://placehold.jp/150x150.png',
-    },
-    {
-      id: 'test5',
-      title: '吾輩は猫である',
-      author: '夏目漱石',
-      imageUrl: 'https://placehold.jp/150x150.png',
-    },
-    {
-      id: 'test6',
-      title: '吾輩は猫である',
-      author: '夏目漱石',
-      imageUrl: 'https://placehold.jp/150x150.png',
-    },
-    {
-      id: 'test7',
-      title: '吾輩は猫である',
-      author: '夏目漱石',
-      imageUrl: 'https://placehold.jp/150x150.png',
-    },
-    {
-      id: 'test8',
-      title: '吾輩は猫である',
-      author: '夏目漱石',
-      imageUrl: 'https://placehold.jp/150x150.png',
-    },
-    {
-      id: 'test9',
-      title: '吾輩は猫である',
-      author: '夏目漱石',
-      imageUrl: 'https://placehold.jp/150x150.png',
-    },
-    {
-      id: 'test10',
-      title: '吾輩は猫である',
-      author: '夏目漱石',
-      imageUrl: 'https://placehold.jp/150x150.png',
-    },
-  ];
-  const urls = novels.map((novel) => `novel/${novel.id}`);
-  return (
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await apiClient.novels.ranking.$get({ query: { limit: 10 } });
+      return res ?? [];
+    };
 
+    fetch().then(setRankings).catch(catchApiErr);
+
+    return () => setRankings([]);
+  }, [catchApiErr]);
+
+  return (
     <div className={styles.container}>
       <div>
         <h1 className={styles.title}>Sakuga AI</h1>
@@ -98,18 +52,19 @@ const Home = () => {
       </div>
       <label className={styles.ranking}>ランキング</label>
       <div>
-        {novels.map((novel, index) => (
-          <Link
-            className={styles[`work${index + 1}` as keyof typeof styles]}
-            href={urls[index]}
-            key={novel.id}
-          >
-            <img src={novel.imageUrl} alt={novel.title} />
-            {novel.title}
-          </Link>
-        ))}
+        {searchInput.length <= 0 &&
+          rankings?.map((novel, i) => (
+            <div key={novel.id}>
+              <p>
+                {i + 1}位: {novel.title}
+              </p>
+              <p>
+                作者: {novel.authorSurname} {novel.authorGivenName}
+              </p>
+              <p>アクセス数: {novel.totalAccessCount}</p>
+            </div>
+          ))}
       </div>
-
     </div>
   );
 };
