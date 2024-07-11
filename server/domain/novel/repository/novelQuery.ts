1995f3110f6b65fcee4d6eef1308f1375b02874b
Yoshiki <s1f102201469@iniad.org>
2024-07-11 19:37:57 +0900

    refactor: Update novelQuery to use ULID for novel IDs

--- server/domain/novel/repository/novelQuery.ts
+++ server/domain/novel/repository/novelQuery.ts
@@ -1,15 +1,45 @@
-import type { Prisma } from '@prisma/client';
+import type { Novel, Prisma } from '@prisma/client';
+import type { EntityId } from 'api/@types/brandedId';
+import type { NovelBodyEntity } from 'api/@types/novel';
+import { paragraphQuery } from 'domain/paragraph/repository/paragraphQuery';
+import { brandedId } from 'service/brandedId';
 
-const getNovelUrlByWorkId = async (
+const toEntity = async (
+  tx: Prisma.TransactionClient,
+  prismaNovel: Novel,
+): Promise<NovelBodyEntity> => ({
+  id: brandedId.novel.entity.parse(prismaNovel.id),
+  workId: prismaNovel.workId,
+  title: prismaNovel.title,
+  authorName: `${prismaNovel.authorSurname} ${prismaNovel.authorGivenName}`,
+  paragraphs: await paragraphQuery.listByNovelId(tx, brandedId.novel.entity.parse(prismaNovel.id)),
+  aozoraUrl: prismaNovel.htmlFileUrl,
+});
+
+const getNovelByWorkId = async (
   tx: Prisma.TransactionClient,
   workId: number,
-): Promise<string | null> => {
+): Promise<NovelBodyEntity | null> => {
   const prismaNovel = await tx.novel.findFirst({
     where: { workId },
   });
   if (prismaNovel === null) return null;
 
-  return prismaNovel.htmlFileUrl;
+  return toEntity(tx, prismaNovel);
 };
 
-export { getNovelUrlByWorkId };
+const getNovelById = async (
+  tx: Prisma.TransactionClient,
+  id: EntityId['novel'],
+): Promise<NovelBodyEntity | null> => {
+  const prismaNovel = await tx.novel.findFirst({
+    where: { id },
+  });
+  if (prismaNovel === null) return null;
+  return toEntity(tx, prismaNovel);
+};
+
+export const novelQuery = {
+  getNovelByWorkId,
+  getNovelById,
+};
