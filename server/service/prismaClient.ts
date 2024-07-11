f162951d1c92e3bf60ce68a797aba78bcd333d8a
Yoshiki <s1f102201469@iniad.org>
2024-07-11 21:45:47 +0900

    feat: Add timeout and maxWait options to transaction function

--- server/service/prismaClient.ts
+++ server/service/prismaClient.ts
@@ -6,8 +6,10 @@ export const transaction = <U>(
   isolationLevel: Prisma.TransactionIsolationLevel,
   fn: (tx: Prisma.TransactionClient) => Promise<U>,
   retry = 3,
+  timeout = 60000,
+  maxWait = 60000,
 ): Promise<U> =>
-  prismaClient.$transaction<U>(fn, { isolationLevel }).catch((e) => {
+  prismaClient.$transaction<U>(fn, { isolationLevel, timeout, maxWait }).catch((e) => {
     if (
       e instanceof Prisma.PrismaClientKnownRequestError &&
       ['P2028', 'P2034'].includes(e.code) &&
