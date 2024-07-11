8d12c097863f319fbb884493ac028c2f94bf1c72
Yoshiki <s1f102201469@iniad.org>
2024-07-12 02:56:31 +0900

    feat: Add getParagraph function to novelUseCase

--- server/domain/novel/useCase/novelUseCase.ts
+++ server/domain/novel/useCase/novelUseCase.ts
@@ -1,4 +1,6 @@
+import type { NovelBodyEntity } from 'api/@types/novel';
 import { load } from 'cheerio';
+import { paragraphUseCase } from 'domain/paragraph/useCase/paragraphUseCase';
 import { decode } from 'iconv-lite';
 import { transaction } from 'service/prismaClient';
 import { novelQuery } from '../repository/novelQuery';
@@ -13,5 +15,11 @@ export const novelUseCase = {
       const $ = load(html);
 
       return $('div.main_text').text();
+    }),
+  getParagraph: async (workId: number): Promise<NovelBodyEntity | null> =>
+    transaction('RepeatableRead', async (tx) => {
+      const novel = await novelQuery.getNovelByWorkId(tx, workId);
+      if (novel === null) return null;
+      return { ...novel, paragraphs: await paragraphUseCase.getOrCreateParagraphs(workId) };
     }),
 };
